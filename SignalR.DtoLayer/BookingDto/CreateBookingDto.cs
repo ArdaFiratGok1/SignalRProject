ebc588808ffd0d74dabad6c21731fff722a02940
ArdaFiratGok1 <firat.gok@std.yildiz.edu.tr>
2025-08-30 14:30:50 +0300

    Dtolar oluşturuldu mapping yapılacak

---
+++ SignalR.DtoLayer/BookingDto/CreateBookingDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DtoLayer.BookingDto
+{
+    public class CreateBookingDto
+    {
+        public string BookingName { get; set; }
+        public string BookingPhone { get; set; }
+        public string BookingMail { get; set; }
+        public int PersonCount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
